e1df875ad919a5f5cd31114d94605a1dc27125f0
Likvik <erik@elfisk.dk>
2025-04-22 11:49:54 +0200

    ball

---
+++ Scripts/movement.gd
@@ -0,0 +1,47 @@
+extends RigidBody2D
+
+
+var speed = 100
+var maxspeed = 300
+var stoppingpower = 5
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("move_up") and self.linear_velocity.y > -1 * maxspeed:
+		var direction = Vector2(0,-speed)
+		self.apply_central_impulse(direction)
+	
+	elif Input.is_action_pressed("move_down") and self.linear_velocity.y < 1 * maxspeed:
+		var direction = Vector2(0,speed)
+		self.apply_central_impulse(direction)
+	
+	else:
+		var direction = Vector2(0,(-1 * linear_velocity.y)/stoppingpower)
+		self.apply_central_impulse(direction)
+	
+	
+	if Input.is_action_pressed("move_left") and self.linear_velocity.x > -1 * maxspeed:
+		var direction = Vector2(-speed,0)
+		self.apply_central_impulse(direction)
+	
+	elif Input.is_action_pressed("move_right") and self.linear_velocity.x < 1 * maxspeed:
+		var direction = Vector2(speed,0)
+		self.apply_central_impulse(direction)
+	
+	else:
+		var direction = Vector2((-1 * linear_velocity.x)/stoppingpower,0)
+		self.apply_central_impulse(direction)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	 
+	pass
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,50 +1,27 @@
-extends RigidBody2D
+extends Node
 
+var range = 100
+var distance
+var direction
+var elasticity = 0.25
+var drag = 5
 
-
-var speed = 60
-var maxspeed = 300
-
-var stoppingpower = 30
+@onready var player = $"%player_character"
+@onready var ball = $"%Ball"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-
-func _physics_process(delta: float) -> void:
-
-	
-	if Input.is_action_pressed("move_up") and self.linear_velocity.y > -1 * maxspeed:
-		var direction = Vector2(0,-speed)
-		self.apply_central_impulse(direction)
-	
-	elif Input.is_action_pressed("move_down") and self.linear_velocity.y < 1 * maxspeed:
-		var direction = Vector2(0,speed)
-		self.apply_central_impulse(direction)
-	
-	else:
-		var direction = Vector2(0,(-1 * linear_velocity.y)/stoppingpower)
-		self.apply_central_impulse(direction)
-	
-	
-	
-	if Input.is_action_pressed("move_left") and self.linear_velocity.x > -1 * maxspeed:
-		var direction = Vector2(-speed,0)
-		self.apply_central_impulse(direction)
-	
-	elif Input.is_action_pressed("move_right") and self.linear_velocity.x < 1 * maxspeed:
-		var direction = Vector2(speed,0)
-		self.apply_central_impulse(direction)
-	
-	else:
-		var direction = Vector2((-1 * linear_velocity.x)/stoppingpower,0)
-		self.apply_central_impulse(direction)
-	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	 
-	pass
+	direction = (player.global_position-ball.global_position).normalized()
+	distance = (player.global_position-ball.global_position).length()
+	
+	if distance > range:
+		
+		ball.apply_central_impulse(direction * elasticity * (distance-range))
+	
+	var drag_direction = -(ball.linear_velocity).normalized()
+	ball.apply_central_impulse(drag_direction*drag)
